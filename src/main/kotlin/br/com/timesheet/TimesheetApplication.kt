ccb31b6bcfd1aeecdb9e91e38590e3ffc5c48ed7
Jonathan <jonathan.moraes6@gmail.com>
2022-01-10 18:51:01 -0300

    Commit inicial do projeto

---
+++ src/main/kotlin/br/com/timesheet/TimesheetApplication.kt
@@ -0,0 +1,11 @@
+package br.com.timesheet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimesheetApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimesheetApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/timesheet/TimesheetApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.timesheet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimesheetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
